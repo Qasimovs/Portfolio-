3e0d457dcd15ccd7a4b23ae0bd0d9f68df186636
mr.qasimovs <senqas.03@gmail.com>
2025-04-09 07:13:41 +0400

    task elave edildi

---
+++ 17-DOMSelectorStyleNavigateInteractiveElements/assets/js/script.js
@@ -0,0 +1,20 @@
+let heart = document.querySelector(".heart").parentNode.nextELementSibling.lastElementChild;
+
+// let title = document.querySelector(".cardTitle");
+// let price = document.querySelector(".cardPrice");
+// let shop = document.querySelector(".shopBtn");
+
+let obj ={
+    image :image,
+    
+    title : title,
+    price : price,
+    shop : shop,
+
+};
+console.log(obj);
+
+
+
+
+
