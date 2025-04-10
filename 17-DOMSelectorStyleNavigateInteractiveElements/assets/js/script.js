5a83a4a500849c0a46b64ab99f78d9778928bfd4
mr.qasimovs <senqas.03@gmail.com>
2025-04-10 07:05:37 +0400

    task elav edildi

--- 17-DOMSelectorStyleNavigateInteractiveElements/assets/js/script.js
+++ 17-DOMSelectorStyleNavigateInteractiveElements/assets/js/script.js
@@ -1,18 +1,46 @@
-let heart = document.querySelector(".heart").parentNode.nextELementSibling.lastElementChild;
+// Task 1.1
 
-// let title = document.querySelector(".cardTitle");
-// let price = document.querySelector(".cardPrice");
-// let shop = document.querySelector(".shopBtn");
+// var btn = document.querySelector(".shopBtn").parentNode.previousSibling.firstElementChild; 
+// var btn = document.querySelector(".shopBtn").parentNode.previousSibling.lastElementChild;
+// var btn = document.querySelector(".shopBtn").parentNode.firstElementChild
+// var btn = document.querySelector(".shopBtn").parentNode.firstElementChild.nextElementSibling
+// var btn = document.querySelector(".shopBtn").previousSibling
+// // let title = document.querySelector(".cardTitle");
+// // let price = document.querySelector(".cardPrice");
+// // let shop = document.querySelector(".shopBtn");
 
-let obj ={
-    image :image,
+// var wishliste ={
+//     heart,
+//     img,    
+//     title,
+//     text,
+//     price,
     
-    title : title,
-    price : price,
-    shop : shop,
+// }
+// console.log(wishliste);
 
-};
-console.log(obj);
+
+
+// Cloest usulu
+let btn=document.querySelector(".shopBtn");
+let card =btn.closest(".card");
+
+let heart =document.querySelector(".heart").textContent;
+let img =document.querySelector(".cardimg").src;
+let title =document.querySelector(".cardTitle").textContent;
+let text =document.querySelector(".cardText").textContent;
+let price =document.querySelector(".cardPrice").textContent;
+
+
+let wishliste ={
+    heart,
+    img,
+    title,
+    text,
+    price,
+    
+}
+console.log(wishliste);
 
 
 
